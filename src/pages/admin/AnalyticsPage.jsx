c1b9a8f57db8732c1cb64b696be72d0e15bf3ed9
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2026-03-10 03:25:33 -0700

    Update AnalyticsPage.jsx

--- src/pages/admin/AnalyticsPage.jsx
+++ src/pages/admin/AnalyticsPage.jsx
@@ -17,6 +17,7 @@ const AnalyticsPage = ({ admin }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     fetchData();
   }, [dateRange]);
